d57e30d71062739b1f9ba93ca012d1de10e76919
CaptainRelief <kazu@tuta.io>
2023-01-11 23:31:48 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
+
+const canvas = document.getElementById("c");
 var world = new CANNON.World();
 
 
